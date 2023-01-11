bb196abf087fef23fe972cf33b2a261caa8ec23a
Julien Cheritat <julien.cheritat@neos-sdi.com>
2023-01-11 12:35:14 +0100

    Race condition

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program {
+  public static void Main() {
+    while (true) {
+      int Counter = 0;
+      const int MAX = 500;
+      var threads = new Thread[MAX];
+      for (int num = 0; num < MAX; num++) {
+        threads[num] = new Thread(new ThreadStart(delegate {
+          Thread.Sleep(1);
+          Counter++;
+        }));
+        threads[num].Start();
+      }
+      for (int num = 0; num < MAX; num++) {
+        threads[num].Join();
+      }
+      Console.WriteLine(Counter);
+    }
+  }
+}
